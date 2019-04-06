770fe6b2edc0dc61240edfbd4bb6582b8f5098df
Duncan Leo <hello@duncanleo.me>
2019-04-07 00:20:29 +0800

    add sample child element

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import '@babel/polyfill';
 
 const rootElement = document.querySelector('#root');
 
+const spanChild = document.createElement('span');
+spanChild.textContent = 'Hello world!';
+
+rootElement.appendChild(spanChild);
